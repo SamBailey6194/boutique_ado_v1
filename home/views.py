d0a538ee6c12e90597022176989a1db3bc85af31
SamBailey6194 <samabailey6194@gmail.com>
2025-09-08 11:45:44 +0100

    Add home app for homepage

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    """
+    A view to render homepage
+    """
+    return render(request, 'home/index.html')
