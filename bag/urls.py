bdab261a80ebd1310e53bc91016e9fea5d6ad5f9
SamBailey6194 <samabailey6194@gmail.com>
2025-09-15 14:52:16 +0100

    Add shopping bag adding functionality

--- bag/urls.py
+++ bag/urls.py
@@ -6,5 +6,6 @@ app_name = 'bag'
 
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
