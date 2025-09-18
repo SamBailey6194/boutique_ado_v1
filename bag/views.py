42803f76fa866cc5fcab794545ffea4851480f79
SamBailey6194 <samabailey6194@gmail.com>
2025-09-18 18:39:11 +0100

    feat(bag): Add update and remove buttons to shopping bag
    Add functional update button that allows user to use the + or - to update the quantity within the shopping bag and then click update to load a bag with the updated quantities and subtotal for that item
    
    Add function remove button that allows the user to remove an item from shopping bag completely and if the iotem has a size it only removes for that size not all items.
    
    This change involved:
    - Updating quantity_input_script.html to use AJAX style jQuery and JS
    - Updating bag/views.py to have remove_bag and adjust_bag views and updating urls.py to match
    -  Ensuring bag.html in the input and remove button we had the data-size

--- bag/views.py
+++ bag/views.py
@@ -1,38 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.urls import reverse
 from products.models import Product
 
 
 # Create your views here.
 def view_bag(request):
-    """
-    A view to render shopping bag
-    """
+    """ Render shopping bag """
     bag = request.session.get('bag', {})
     bag_items = []
+
+    total = 0
 
     for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
 
         if product.has_sizes:
             for size, quantity in item_data.get('items_by_size', {}).items():
+                subtotal = product.price * quantity
+                total += subtotal
                 bag_items.append({
                     'item_id': item_id,
                     'product': product,
                     'quantity': quantity,
                     'size': size,
-                    'subtotal': product.price * quantity
+                    'subtotal': subtotal
                 })
         else:
             quantity = item_data if isinstance(item_data, int) else 0
+            subtotal = product.price * quantity
+            total += subtotal
             bag_items.append({
                 'item_id': item_id,
                 'product': product,
                 'quantity': quantity,
-                'subtotal': product.price * quantity
+                'subtotal': subtotal
             })
 
     context = {
         'bag_items': bag_items,
+        'total': total,
+        'delivery': 0,  # or calculate delivery
+        'grand_total': total,  # total + delivery
     }
     return render(request, 'bag/bag.html', context)
 
@@ -73,3 +82,56 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity of an item in the bag via AJAX """
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity', 1))
+    size = request.POST.get('product_size', None)
+    item_id = str(item_id)
+
+    bag = request.session.get('bag', {})
+
+    if product.has_sizes and size:
+        if item_id in bag and 'items_by_size' in bag[item_id]:
+            if quantity > 0:
+                bag[item_id]['items_by_size'][size] = quantity
+            else:
+                bag[item_id]['items_by_size'].pop(size)
+                if not bag[item_id]['items_by_size']:
+                    bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id, None)
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag:view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        item_id = str(item_id)
+        bag = request.session.get('bag', {})
+
+        # Accept either 'size' or 'product_size' from POST
+        size = request.POST.get('size') or request.POST.get('product_size')
+
+        if size:
+            if item_id in bag and 'items_by_size' in bag[item_id]:
+                bag[item_id]['items_by_size'].pop(size, None)
+                if not bag[item_id]['items_by_size']:
+                    bag.pop(item_id, None)
+        else:
+            bag.pop(item_id, None)
+
+        print("Debug: Item removed from bag after click")
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception:
+        return HttpResponse(status=500)
