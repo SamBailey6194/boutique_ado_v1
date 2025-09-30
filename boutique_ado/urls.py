9c2d6b08192835cf055c91ea71cb71a805290bd5
SamBailey6194 <samabailey6194@gmail.com>
2025-09-30 09:24:42 +0100

    Created checkout app and models

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('bag/', include('bag.urls')),
+    # path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
