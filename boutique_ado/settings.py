7015676bab58fb9797b4ffd0a297124f2099d9b6
SamBailey6194 <samabailey6194@gmail.com>
2025-09-30 10:22:16 +0100

    Create html for checkout form and add crispy forms bootstrap 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout.apps.CheckoutConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -76,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
