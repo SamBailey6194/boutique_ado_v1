8277ffd00eab0f81eba0f54f7854781f01a1f90a
SamBailey6194 <samabailey6194@gmail.com>
2025-10-13 10:43:10 +0100

    Add html for add product page

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
