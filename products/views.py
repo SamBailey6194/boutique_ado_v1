b65f69fedf97004d82aac25edfa359eb5045f537
SamBailey6194 <samabailey6194@gmail.com>
2025-10-13 11:44:59 +0100

    Add links and messages for add product

--- products/views.py
+++ products/views.py
@@ -82,6 +82,19 @@ def add_product(request):
     """
     Add a product to the store
     """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('products:add_product'))
+        else:
+            messages.error(
+                request, 'Failed to add product. Please ensure form is valid'
+                )
+    else:
+        form = ProductForm()
+
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
