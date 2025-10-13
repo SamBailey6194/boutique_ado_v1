2294153377bd6a7bee4495f5ac05ed5c1b24cb2d
SamBailey6194 <samabailey6194@gmail.com>
2025-10-13 21:58:35 +0100

    Update custom_storage.py

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,16 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = getattr(
+            settings, 'STATICFILES_LOCATION', 'static'
+            )
+        super().__init__(*args, **kwargs)
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = getattr(
+            settings, 'MEDIAFILES_LOCATION', 'media'
+            )
+        super().__init__(*args, **kwargs)
