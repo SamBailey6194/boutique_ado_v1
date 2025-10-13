bf2570bc018f80a096015eb2c67ebfd02cd5c6d4
SamBailey6194 <samabailey6194@gmail.com>
2025-10-13 22:58:18 +0100

    Add / at the end of staticfiles and mediafiles

--- custom_storages.py
+++ custom_storages.py
@@ -9,7 +9,6 @@ class StaticStorage(S3Boto3Storage):
     Ensures files are publicly readable.
     """
     location = getattr(settings, 'STATICFILES_LOCATION', 'static')
-    default_acl = 'public-read'
     # Optional: prevent S3 from adding its own ACL if using bucket policy
     file_overwrite = True
     querystring_auth = False  # ensures URLs are clean without signed params
@@ -23,5 +22,5 @@ class MediaStorage(S3Boto3Storage):
     """
     location = getattr(settings, 'MEDIAFILES_LOCATION', 'media')
     default_acl = 'public-read'
-    file_overwrite = True
+    file_overwrite = False
     querystring_auth = False
--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from custom_storages import MediaStorage
 
-# Create your models here.
+
+class MyModel(models.Model):
+    file = models.FileField(storage=MediaStorage(), upload_to='uploads/')
+
+    def __str__(self):
+        return self.file.name
