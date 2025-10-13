8e267fdeb3abf5cddb9d0d469aae749c341fb175
SamBailey6194 <samabailey6194@gmail.com>
2025-10-13 22:03:46 +0100

    Update custom storage

--- custom_storages.py
+++ custom_storages.py
@@ -1,18 +1,12 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    def __init__(self, *args, **kwargs):
-        kwargs['location'] = getattr(
-            settings, 'STATICFILES_LOCATION', 'static'
-            )
-        super().__init__(*args, **kwargs)
+    location = 'static'
+    default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
-    def __init__(self, *args, **kwargs):
-        kwargs['location'] = getattr(
-            settings, 'MEDIAFILES_LOCATION', 'media'
-            )
-        super().__init__(*args, **kwargs)
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
